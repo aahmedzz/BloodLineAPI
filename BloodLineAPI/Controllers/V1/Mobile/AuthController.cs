3d03f12db236e25539ec7edc3d59c4f81360364b
Admin <Admin@SALMAAMRO>
2026-03-26 16:29:08 +0200

    Login

--- BloodLineAPI/Controllers/V1/Mobile/AuthController.cs
+++ BloodLineAPI/Controllers/V1/Mobile/AuthController.cs
@@ -1,12 +1,13 @@
 using Asp.Versioning;
+using BloodLineAPI.Application.Common.Interfaces;
+using BloodLineAPI.Application.Features.Auth.Commands.LoginMobileUser;
 using BloodLineAPI.Application.Features.Auth.Commands.RefreshToken;
 using BloodLineAPI.Application.Features.Auth.Commands.RegisterMobileUser;
 using BloodLineAPI.Attributes;
 using MediatR;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using BloodLineAPI.Application.Common.Interfaces;
+using System.Security.Claims;   
 
 namespace BloodLineAPI.Controllers.V1.Mobile;
 
@@ -18,6 +19,7 @@ public class AuthController(ISender sender, IJwtGenerator jwtGenerator) : Contro
 {
     private readonly ISender _sender = sender;
     private readonly IJwtGenerator _jwtGenerator = jwtGenerator;
+   
 
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterMobileUserCommand command, CancellationToken cancellationToken)
@@ -30,7 +32,16 @@ public class AuthController(ISender sender, IJwtGenerator jwtGenerator) : Contro
 
         return Ok(result.Data);
     }
-
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginMobileUserCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(command, cancellationToken);
+        if (!result.IsSuccess)
+        {
+            return Problem(title: "Bad Request", detail: result.Error, statusCode: StatusCodes.Status400BadRequest);
+        }
+        return Ok(result.Data);
+    }
     [HttpPost("refresh-token")]
     public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenCommand command, CancellationToken cancellationToken)
     {
