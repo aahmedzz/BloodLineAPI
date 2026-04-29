65d288a6d17e98bbc00366278b48a18ad98ac314
ahmed nasr <an899645@gmail.com>
2026-04-29 23:59:42 +0300

    Rename prescreening column for dental procedure question MIgrations

---
+++ BloodLineAPI.Infrastructure/Migrations/20260428190945_UpdatePrescreening.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodLineAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePrescreening : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasReceivedBloodTransfusionWithinPastYear",
+                table: "HealthPreScreenings",
+                newName: "HasDentalProcedureInPastWeek");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasDentalProcedureInPastWeek",
+                table: "HealthPreScreenings",
+                newName: "HasReceivedBloodTransfusionWithinPastYear");
+        }
+    }
+}
